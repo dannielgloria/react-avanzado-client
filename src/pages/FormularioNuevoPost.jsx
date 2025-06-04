4f11257bf8d7cdaedeb812d33c1add8250adc42b
dannielgloria <danniel.gloria@gmail.com>
2025-06-04 15:28:58 -0600

    feat: se agregan validaciones con css a nuestro formulario

--- src/pages/FormularioNuevoPost.jsx
+++ src/pages/FormularioNuevoPost.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import './errors.css';
+import {  useForm } from "react-hook-form";
 
 export default function SimpleForm() {
     const { register, handleSubmit, formState: {errors} } = useForm();
+
+    const [email, setEmail]= useState('');
     
     const onSubmit = data => {
         console.log(data);
@@ -10,7 +13,18 @@ export default function SimpleForm() {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input placeholder="Nombre" {...register("nombre")}/>
+            
+            <input value={email}  onChange={(e) => setEmail(e.target.value)}/>
+
+            <input placeholder="Nombre" className={errors.nombre ? 'input-error' : ''}
+            {...register("nombre", {required: true, maxLength: 15})}/>
+            {errors.nombre && (
+                <span className="error-message">
+                    {errors.nombre?.type === 'required'
+                    ? 'Nombre obligatorio'
+                    : 'Debe tener menos de 15 caracteres'}
+                </span>
+            )}
             <input placeholder="Apellido Paterno" {...register("apat")} />
             <input placeholder="Título" {...register("titulo", { required: true,minLength:5})} />
             
@@ -18,6 +32,7 @@ export default function SimpleForm() {
             
             {errors.titulo?.type === 'required' && <p>El título es obligatorio</p>} 
             {errors.titulo?.type === 'minLength' && <p>El título debe tener almenos 5  caracteres</p>} 
+
 
             <button>Enviar</button>
         </form>
