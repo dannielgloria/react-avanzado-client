cbc1729927b42cc573ea6a0b6b05385903edcddb
dannielgloria <danniel.gloria@gmail.com>
2025-06-03 20:01:02 -0600

    feat: se agregan validaciones a nuestro formulario

--- src/pages/FormularioNuevoPost.jsx
+++ src/pages/FormularioNuevoPost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function SimpleForm() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: {errors} } = useForm();
     
     const onSubmit = data => {
         console.log(data);
@@ -12,6 +12,13 @@ export default function SimpleForm() {
         <form onSubmit={handleSubmit(onSubmit)}>
             <input placeholder="Nombre" {...register("nombre")}/>
             <input placeholder="Apellido Paterno" {...register("apat")} />
+            <input placeholder="Título" {...register("titulo", { required: true,minLength:5})} />
+            
+            {errors.titulo && <p>El título debe tener almenos 5  caracteres</p>} 
+            
+            {errors.titulo?.type === 'required' && <p>El título es obligatorio</p>} 
+            {errors.titulo?.type === 'minLength' && <p>El título debe tener almenos 5  caracteres</p>} 
+
             <button>Enviar</button>
         </form>
     );
