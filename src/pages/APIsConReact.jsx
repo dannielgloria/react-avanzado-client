ef59db65f0fbdcd882a6a19caf18133583904dbe
dannielgloria <danniel.gloria@gmail.com>
2025-06-05 20:10:23 -0600

    feat: manejo de errores y carga de apis

--- src/pages/APIsConReact.jsx
+++ src/pages/APIsConReact.jsx
@@ -10,7 +10,7 @@ export default function Demo() {
         <h1>Hola mundo</h1>
     </div>
 }
-*/
+
 
 export default function ListaPublicaciones(){
     const [ publicaciones, setPublicaciones ] = useState([]);
@@ -20,6 +20,42 @@ export default function ListaPublicaciones(){
         .then(response => response.json())
         .then(data => setPublicaciones(data))
     }, [])
+
+    return (
+        <div>
+            <h2>Publicaciones</h2>
+            <ul>
+                {publicaciones.map(publicacion =>(
+                    <li key={publicacion.id}>{publicacion.title}</li>
+                )
+
+                )}
+            </ul>
+        </div>
+    );
+}
+*/
+
+//? Manejo de errores y espera de datos (loading)
+
+export default function ListaPublicaciones(){
+    const [ publicaciones, setPublicaciones ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ error, setError ] = useState(null)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=12')
+        .then(response => {
+            if (!response.ok) throw new Error('Error en la petición');
+            return response.json();
+        })
+        .then(data => setPublicaciones(data))
+        .catch(err => setError(err.message))
+        .finally(() => setIsLoading(false))
+    }, [])
+
+    if (isLoading) return <p>Cargando publicaciones...</p>
+    if (error) return <p>Error: {error}</p> 
 
     return (
         <div>
